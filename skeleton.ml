371ec856d385a8ffd489db739232ce43d161aba6
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2013-07-17 12:43:50 +0200

    (a bit) better interfacing

--- skeleton.ml
+++ skeleton.ml
@@ -1,8 +1,6 @@
 (* Step one: load  the cmts, turn them to lambda *)
 (* Now we just load them, merging later *)
 
-let () = print_endline "Step 1"
-
 (* a little clean could make it simpler *)
 let lambdas =
   Arguments.iterate
@@ -11,21 +9,25 @@ let lambdas =
 	( tree, Typedtree.Tcoerce_none)
     )
 
-(* Step two: merge the lambda code
-   - removing the "global" primitive
-   - putting all functions on top of everything
-*)
-
-let () = print_endline "Step 2"
-
-let lambda = Unglobalize.unglobalize lambdas
-
-(* for debugging purpose, this shall not stay here in final code *)
 let () =
-  if Arguments.print ()
-  then Printlambda.lambda Format.std_formatter lambda;
-  if Arguments.comp ()
-  then Compilelambda.comp lambda;
+  if lambdas = [| |]
+  then print_endline "You must specify at least one cmt file"
+  else
+    begin
 
-(* Step three: analysis *)
-(* (* Step four: compile ? *) *)
+      (* Step two: merge the lambda code
+	 - removing the "global" primitive
+	 - putting all functions on top of everything
+      *)
+
+      let lambda = Unglobalize.unglobalize lambdas in
+
+      (* for debugging purpose, this shall not stay here in final code *)
+      if Arguments.print ()
+      then Printlambda.lambda Format.std_formatter lambda;
+      if Arguments.comp ()
+      then Compilelambda.comp lambda;
+
+    (* Step three: analysis *)
+    (* (* Step four: compile ? *) *)
+    end
